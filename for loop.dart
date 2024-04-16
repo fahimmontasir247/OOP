726902435d616a9c2942dbbb50d8d25de3607d97
fahim <mdfahimmontasir03@gmail.com>
2024-04-16 23:33:13 +0600

    add for loop.dart

---
+++ for loop.dart
@@ -0,0 +1,15 @@
+void main (){
+// i++   -->  i = i+1;
+// i--   -->  i = i-1;
+// i+=   -->  i = i+2
+
+  for (int i = 1; i <10 ; i+=2){
+    print('Fahim Montasir  $i');
+  }
+  // for in loop 
+
+  String name = ' Fahim ';
+  for ( int i = 1; i < name.length ; i ++){
+    print(name[i]);
+  }
+}
