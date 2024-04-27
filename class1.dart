6f7f53d7f1a63067ba2a7c2e5e988f1297397837
fahim <mdfahimmontasir03@gmail.com>
2024-04-27 22:58:27 +0600

    add class1.dart

---
+++ class1.dart
@@ -0,0 +1,14 @@
+ void main (){
+  print(cake().Shape);
+  print(cake().size);
+ }
+ class cake {
+  // variables
+  String Shape = 'carcle';
+  int size = 2; //pound
+
+  //methods
+  void braking(){
+    print ('Braking has start');
+  }
+ }
