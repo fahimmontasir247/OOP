5866fcebeee9d0815f098cd6326e1a1c0efb89d1
fahim <mdfahimmontasir03@gmail.com>
2024-05-12 02:00:51 +0600

    add class3.dart

---
+++ class3.dart
@@ -0,0 +1,22 @@
+void main (){
+final Cake = cake();
+Cake.Shape = 'Rectangle';
+print(Cake.Shape);
+print(cake().Shape);
+}
+class cake {
+  // variable
+   String Shape = 'Circle';
+   double size = 2; // pound
+  //method 
+  void Breaking (){
+    print ('Cooking is started');
+  }
+  bool IsCooling (){
+    return true;
+  }
+}
+/*
+output -- Rectangle
+          Circle
+ */
